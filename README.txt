b96450c13b674af20a64d633160411456c521ecc
ManuelCanelon <manucanelonrojas@gmail.com>
2020-11-25 21:31:15 +0000

    First commit: This commit include app to sum two numbers only.

---
+++ README.txt
@@ -0,0 +1,3 @@
+Project_1 is an application used to sum two numbers.
+
+Input has to be limited to numbers for the app to work.
---
+++ project_1_sum.py
@@ -0,0 +1,5 @@
+print("Welcome to Project_1: This is an app to sum two number values.")
+a = input("Please enter the first number: ")
+b = input("\nPlease enter the second number: ")
+c = a + b
+print(str(a) + " + "  + str(b) + " = " str(c))

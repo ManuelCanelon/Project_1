e4984ebf7d40c33b8cd2ebdf7162a94ea4ef73a6
ManuelCanelon <manucanelonrojas@gmail.com>
2020-11-25 22:43:50 +0000

    Second commit: Added subtraction functionality

---
+++ project_1_subtraction.py
@@ -0,0 +1,5 @@
+print("This app is to subtract two numbers.")
+a = input("\nPlease enter the first number: ")
+b = input("\nPlease enter the second number: ")
+c = a - b
+print(str(a) + " - " + str(b) + " = " + str(c))

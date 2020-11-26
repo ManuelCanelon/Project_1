0ce23b2679e86c90424c44edc0d7ebeb4aad6803
ManuelCanelon <73437973+ManuelCanelon@users.noreply.github.com>
2020-11-26 16:41:51 -0700

    Delete project_1_subtraction.py

--- project_1_subtraction.py
+++
@@ -1,5 +0,0 @@
-print("This app is to subtract two numbers.")
-a = input("\nPlease enter the first number: ")
-b = input("\nPlease enter the second number: ")
-c = a - b
-print(str(a) + " - " + str(b) + " = " + str(c))
